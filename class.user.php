1102467962d1927f63f579de4e67e566028d8c3f
fatihkayaci <f.kayaci@outlook.com.tr>
2023-12-18 16:18:36 +0300

    asd

--- class.user.php
+++ class.user.php
@@ -118,10 +118,10 @@ class USER
   $mail->Host       = "smtp.gmail.com";      
   $mail->Port       = 465;             
   $mail->AddAddress($email);
-  $mail->Username="celalyl555@gmail.com";  
-  $mail->Password="qcpnwelmoioogojz";            
-  $mail->SetFrom('celalyl555@gmail.com','Coding Cage');
-  $mail->AddReplyTo("celalyl555@gmail.com","Coding Cage");
+  $mail->Username="fatihkayaci5334@gmail.com";  
+  $mail->Password="opkxknlhpcbmztwt";            
+  $mail->SetFrom('fatihkayaci5334@gmail.com','Coding Cage');
+  $mail->AddReplyTo("fatihkayaci5334@gmail.com","Coding Cage");
   $mail->Subject    = $subject;
   $mail->MsgHTML($message);
   $mail->Send();
