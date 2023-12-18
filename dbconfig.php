494efc8cf6186e5f69dd8e0dcc952f93ab3a5b8f
celalyl555 <74668113+celalyl555@users.noreply.github.com>
2023-12-18 15:17:06 +0300

    celal

---
+++ dbconfig.php
@@ -0,0 +1,28 @@
+<?php
+class Database
+{
+     
+    private $host = "45.10.151.41";
+    private $db_name = "katsis";
+    private $username = "root";
+    private $password = "ELlggUcQi62HjoAZ";
+    public $conn;
+     
+    public function dbConnection()
+ {
+     
+     $this->conn = null;    
+        try
+  {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+   $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        }
+  catch(PDOException $exception)
+  {
+            echo "Connection error: " . $exception->getMessage();
+        }
+         
+        return $this->conn;
+    }
+}
+?>
