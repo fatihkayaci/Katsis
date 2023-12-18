82e9381b8472f59102b903a7f2ae8ddd86a1fc78
celalyl555 <74668113+celalyl555@users.noreply.github.com>
2023-12-18 18:32:57 +0300

    .php uzantısı kaldırıldı

--- index.php
+++ index.php
@@ -71,8 +71,8 @@ if(isset($_POST['btn-login']))
         <input type="password" class="input-block-level" placeholder="Password" name="txtupass" required />
       <hr />
         <button class="btn btn-large btn-primary" type="submit" name="btn-login">Sign in</button>
-        <a href="signup.php" style="float:right;" class="btn btn-large">Sign Up</a><hr />
-        <a href="fpass.php">Lost your Password ? </a>
+        <a href="signup" style="float:right;" class="btn btn-large">Sign Up</a><hr />
+        <a href="fpass">Lost your Password ? </a>
       </form>
 
     </div> <!-- /container -->
