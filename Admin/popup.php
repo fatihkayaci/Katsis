7e71265821ff6f3bc7bb5a65d94048f0569ab197
celalyl555 <74668113+celalyl555@users.noreply.github.com>
2023-12-20 19:17:49 +0300

    bloka sayısı eklendi

--- Admin/popup.php
+++ Admin/popup.php
@@ -15,12 +15,13 @@
 							<input type="text" class="form-control" id="name" name="name"  required>
 						</div>
 						<div class="form-group col-md-2">
-							<label for="name">blok sayısı</label>
-							<input type="number" class="form-control" id="name" name="name" required>
+							<label for="blokSay">blok sayısı</label>
+							<input type="number" class="form-control" id="blokSay" name="blokSay" value="1" max="20" min="1" required>
+							<p id="numberalert"></p>
 						</div>
 					</div>
-         
-					
+					<div class="row" id ="table-container">
+					</div>
 			<!--		<div class="form-check">
 						<label>
 						<input type="checkbox" name="formcheck" checked="" class="checkfz" required=""> Submit<br>
@@ -33,6 +34,74 @@
 
 
         <script>
+
 $('.form-popup').show();
 
+    // HTML sayfasındaki input alanını seç
+    var inputElement = document.getElementById('blokSay');
+	var numberalert = document.getElementById('numberalert');
+	var tableContainer = document.getElementById('table-container'); // oluşacak tabloyu çağırıyoruz
+	var enteredNumber = inputElement.value;
+	tableCreate(enteredNumber);
+    // Input alanındaki değeri değiştikçe bu fonksiyonu çağır
+    inputElement.addEventListener('input', function() {
+        // Input alanındaki değeri al
+        tableCreate(enteredNumber);
+		var enteredNumber = inputElement.value;
+		if(enteredNumber > 20){
+			numberalert.innerHTML="20'den fazla blok bulunuyorsa iletişime geçiniz";
+			enteredNumber.innerHTML=20;
+			tableContainer.innerHTML = ''; 
+		}else if(enteredNumber == ""){
+			numberalert.innerHTML="";
+		}
+		else if(enteredNumber <= 0){
+			numberalert.innerHTML="En az 1 blok ekleyiniz.";
+			tableContainer.innerHTML = ''; 
+		}
+		else{
+			numberalert.innerHTML="";
+			tableCreate(enteredNumber);
+		}
+        // Alınan değeri konsola yazdır
+        console.log('Girilen Sayı: ' + enteredNumber);
+    });
+
+
+
+	function tableCreate(rowCount) {
+
+	
+        tableContainer.innerHTML = ''; // Temizleme işlemi, her çağrıda tabloyu sıfırlar.
+
+        // Tablo oluşturma
+        var table = document.createElement('table');
+        table.setAttribute('border', '1');
+
+        // Başlık satırı ekleme
+        var headerRow = table.insertRow(0);
+        var blockHeader = headerRow.insertCell(0);
+        blockHeader.innerHTML = '<b>Blok</b>';
+
+        var apartmentHeader = headerRow.insertCell(1);
+        apartmentHeader.innerHTML = '<b>Daire Sayısı</b>';
+
+        // Satırları oluşturma
+        for (var i = 1; i <= rowCount; i++) {
+            var row = table.insertRow(i);
+            var blockCell = row.insertCell(0);
+            blockCell.innerHTML = i;
+
+            var apartmentCell = row.insertCell(1);
+            var inputElement = document.createElement('input');
+            inputElement.type = 'number';
+            inputElement.className = 'form-control';
+            inputElement.name = 'daireSayisi';
+            inputElement.required = true;
+            apartmentCell.appendChild(inputElement);
+        }
+
+        tableContainer.appendChild(table);
+		
+	}
 </script>
