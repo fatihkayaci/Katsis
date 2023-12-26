c5790d084aee07d7b5a74058e9401ae9ed9751cf
fatihkayaci <f.kayaci@outlook.com.tr>
2023-12-26 11:15:40 +0300

    auto-commit

--- Admin/Accounts/index.php
+++ Admin/Accounts/index.php
@@ -1,6 +1,4 @@
-<?php
 
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +6,93 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+    <style>
+        tfoot input {
+        width: 100%;
+        padding: 3px;
+        box-sizing: border-box;
+    }
+    </style>
 </head>
 <body>
-    
+<table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011-04-25</td>
+                <td>$320,800</td>
+            </tr>
+           
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </tfoot>
+    </table>
 </body>
+<?php
+
+$sql = "SELECT * FROM tbl_users";
+$result = $conn->query($sql);
+if ($result->> 0) {
+    echo "<table id='example' class='display' style='width:100%'>";
+    echo "<tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["userID"]. "</td><td>" . $row["userName"]. "</td><td>" . $row["userEmail"]. "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 sonuç";
+}
+
+
+$conn->close();
+?>
+
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script>
+   new DataTable('#example', {
+    initComplete: function () {
+        this.api()
+            .columns()
+            .every(function () {
+                let column = this;
+                let title = column.footer().textContent;
+ 
+                // Create input element
+                let input = document.createElement('input');
+                input.placeholder = title;
+                column.footer().replaceChildren(input);
+ 
+                // Event listener for user input
+                input.addEventListener('keyup', () => {
+                    if (column.search() !== this.value) {
+                        column.search(input.value).draw();
+                    }
+                });
+            });
+    }
+});
+</script>
 </html>
