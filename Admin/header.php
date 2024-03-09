d6a25029f48042f5d8f79ab53083faf55f932e73
yusufft <yusufft2000@gmail.com>
2024-03-09 19:53:02 +0300

    yeni

--- Admin/header.php
+++ Admin/header.php
@@ -7,6 +7,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="assets/img/ico.png">
   <link rel="icon" type="image/png" href="assets/img/ico.png">
   <title>Katsis</title>
+
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Font Awesome Icons -->
