4e4e520e51bceeeeae9678de46c13dc2e4962946
yusufft <yusufft2000@gmail.com>
2024-05-20 16:58:42 +0300

    a

--- Admin/Controller/pdf/pdf_template1.php
+++ Admin/Controller/pdf/pdf_template1.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
 
 
-    <title>Invoice with ribbon - Bootdey.com</title>
+    <title>Fatura</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <style type="text/css">
